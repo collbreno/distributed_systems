95f5f52558635f375463d0e6bfd575c4b9dbcdbd
collbreno <collbreno@dcc.ufrj.br>
2021-08-01 21:44:53 -0300

    Adding lab 2

---
+++ lab_2/client.py
@@ -0,0 +1,35 @@
+import socket
+
+HOST = 'localhost' # maquina onde esta o par passivo
+PORTA = 5000        # porta que o par passivo esta escutando
+EXIT_CODE = '$exit'
+ENCODING = 'utf-8'
+
+# Trata o input para não aceitar palavra vazia
+def read_input(label: str = None):
+    while True:
+        txt = input() if label is None else input(label)
+        if len(txt) != 0:
+            return txt
+
+
+sock = socket.socket() # default: socket.AF_INET, socket.SOCK_STREAM 
+sock.connect((HOST, PORTA)) 
+
+while True:
+    # Solicita o input do usuário
+    file_name = read_input(f'Digite o nome do arquivo (ou "{EXIT_CODE}" para encerrar): ')
+    if file_name == EXIT_CODE:
+        break
+    search_query = read_input('Digite a palavra a ser buscada: ')
+
+    # Envia as duas mensagens para o servidor
+    sock.send(str.encode(file_name, encoding=ENCODING))
+    sock.send(str.encode(search_query, encoding=ENCODING))
+
+    # Imprime a resposta
+    msg = sock.recv(1024)
+    print(str(msg,  encoding=ENCODING))
+
+# encerra a conexao
+sock.close() 
